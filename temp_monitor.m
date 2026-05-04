13ceb7682575a475778a02935df1e22d7490e28a
wspike789-cloud <wspike789@gmail.com>
2026-05-05 00:04:27 +0800

    temp monitor

--- temp_monitor.m
+++ temp_monitor.m
@@ -6,4 +6,14 @@ function temp_monitor(a)
     counter = 0;      
     led_state = 0;    
     
-   
+    figure('Name', 'Live Temperature Monitor');
+    h_plot = plot(NaN, NaN, 'b-', 'LineWidth', 1.5);
+    title('Real-time Capsule Temperature');
+    xlabel('Time (s)');
+    ylabel('Temperature (^{\circ}C)');
+    grid on;
+    
+    disp('Monitoring started. Press Ctrl+C in Command Window to stop.');
+
+    
+    
