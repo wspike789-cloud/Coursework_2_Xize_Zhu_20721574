b548f781d92dbd7308515063cb75916954c4fa2c
wspike789-cloud <wspike789@gmail.com>
2026-05-05 00:28:23 +0800

    temp monitor

--- temp_monitor.m
+++ temp_monitor.m
@@ -15,5 +15,37 @@ function temp_monitor(a)
     
     disp('Monitoring started. Press Ctrl+C in Command Window to stop.');
 
+    while true
+        
+        pause(0.25); 
+        counter = counter + 1;
+        
+        
+        if mod(counter, 4) == 0
+          
+            v_out = readVoltage(a, 'A0');
+            
+            current_temp = (v_out - 0.5) / 0.01; 
+            
+            current_time = toc(start_time); 
+           
+            time_data = [time_data, current_time];
+            temp_data = [temp_data, current_temp];
+            
+            set(h_plot, 'XData', time_data, 'YData', temp_data);
+            
+            if current_time > 20
+                xlim([current_time - 20, current_time + 5]);
+            else
+                xlim([0, 25]);
+            end
+            drawnow; 
+        end
+
+        if isempty(temp_data)
+            continue; 
+        end
+        
+        
     
     
