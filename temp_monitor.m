452ecc14ddb78bfe86d96bb62d26a2bb7f37dc5f
wspike789-cloud <wspike789@gmail.com>
2026-05-05 12:02:40 +0800

    temp monitor

--- temp_monitor.m
+++ temp_monitor.m
@@ -1,51 +1,12 @@
 function temp_monitor(a)
 
+
+
     time_data = [];
     temp_data = [];
-    start_time = tic; 
+    start_time = tic;
     counter = 0;      
-    led_state = 0;    
+    led_state = 0;   
     
-    figure('Name', 'Live Temperature Monitor');
-    h_plot = plot(NaN, NaN, 'b-', 'LineWidth', 1.5);
-    title('Real-time Capsule Temperature');
-    xlabel('Time (s)');
-    ylabel('Temperature (^{\circ}C)');
-    grid on;
-    
-    disp('Monitoring started. Press Ctrl+C in Command Window to stop.');
 
-    while true
-        
-        pause(0.25); 
-        counter = counter + 1;
-        
-        
-        if mod(counter, 4) == 0
-          
-            v_out = readVoltage(a, 'A0');
-            
-            current_temp = (v_out - 0.5) / 0.01; 
-            
-            current_time = toc(start_time); 
-           
-            time_data = [time_data, current_time];
-            temp_data = [temp_data, current_temp];
-            
-            set(h_plot, 'XData', time_data, 'YData', temp_data);
-            
-            if current_time > 20
-                xlim([current_time - 20, current_time + 5]);
-            else
-                xlim([0, 25]);
-            end
-            drawnow; 
-        end
-
-        if isempty(temp_data)
-            continue; 
-        end
-        
-        
-    
     
